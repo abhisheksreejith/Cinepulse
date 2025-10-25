149f800d720b32e3b3cac77b0102f3e04d81f5e7
Abhishek C Sreejith <abhishek.sreejith@7edge.com>
2025-10-25 18:17:52 +0530

    Initial Commit

---
+++ Cinepulse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinepulse
+//
+//  Created by Abhishek C Sreejith on 25/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
